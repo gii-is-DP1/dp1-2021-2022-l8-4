d513851c7a66c4a5013f7cbae285219d7c99d00d
lNoelia <noelialopezduran@gmail.com>
2021-10-27 17:13:26 +0200

    Revisar, /cards white page error

--- src/main/java/org/springframework/samples/petclinic/card/CardController.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardController.java
@@ -7,33 +7,39 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Jose Maria Delgado Sanchez
+ * @author Noelia López Durán
  */
 @Controller
+@RequestMapping("/cards")
 public class CardController {
     
-    private static final String VIEWS_CARD_CREATE_OR_UPDATE_FORM = "cards/createOrUpdateCardForm";
-
-    private final CardService cardService;
+//  private static final String VIEWS_CARD_CREATE_OR_UPDATE_FORM = "cards/createOrUpdateCardForm";
 
     @Autowired
-    public CardController(CardService cardService){
-        this.cardService = cardService;
-    }
+    private CardService cardService;
 
-    @InitBinder
+    public String listadoCards(ModelMap modelmap){
+        String vista ="cards/listadoCards";
+        Iterable<Card> cards= cardService.findAll();
+        modelmap.addAttribute("cards", cards);
+        return vista;
+    }
+    /*@InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
-    }
+    }*/
 
-    @GetMapping(value = "/cards/new")
+    /*@GetMapping(value = "/cards/new")
     public String initCreationForm(Map<String, Object> model) {
         Card card = new Card();
         model.put("card", card);
@@ -50,13 +56,13 @@ public class CardController {
 
             return "redirect:/cards/" + card.getId();
         }
-    }
-
+    }*/
+/*
     @GetMapping(value = "/cards")
     public String processFindForm(Card card, BindingResult result, Map<String, Object> model) {
 
-        Collection<Card> results = this.cardService.findAll();
-        if(results.isEmpty()){
+        Iterable<Card> results = this.cardService.findAll();
+        if(results.){
             //no cards found
             result.rejectValue("notFound", "not found");
             return "cards";
@@ -64,5 +70,5 @@ public class CardController {
             model.put("selections", results);
             return "cards/cardsList";
         }
-    }
+    }*/
 }
--- src/main/java/org/springframework/samples/petclinic/card/CardService.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -1,10 +1,5 @@
 package org.springframework.samples.petclinic.card;
 
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -15,23 +10,29 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 public class CardService {
-    
-    private CardRepository cardRepository;
 
     @Autowired
+    private static CardRepository cardRepository;
+
+    /*@Autowired
     public CardService(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
-    }
+    }*/
 
-    @Transactional(readOnly = true)
-    public Collection<Card> findAll() throws DataAccessException {
-        return StreamSupport.stream(cardRepository.findAll().spliterator(), false)
-                            .collect(Collectors.toList()); 
+    @Transactional //(readOnly = true)
+    public Iterable<Card> findAll(){
+        Iterable<Card> res = cardRepository.findAll();
+        return res;
     }
 
     @Transactional
+    public int cardCount(){
+        return (int) cardRepository.count();
+    }
+
+    /*@Transactional
     public void saveCard(Card card) throws DataAccessException {
         //creating card
         cardRepository.save(card);
-    }
+    }*/
 }
