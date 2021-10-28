b8de873cd296091d9a5874a99c3695b1a812071c
lNoelia <noelialopezduran@gmail.com>
2021-10-28 13:27:44 +0200

    Limpieza de comentarios

--- src/main/java/org/springframework/samples/petclinic/card/CardController.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.card.exceptions.DuplicatedCardNameException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
