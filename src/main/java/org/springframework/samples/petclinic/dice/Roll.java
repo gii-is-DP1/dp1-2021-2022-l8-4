eb12c5e0e5771da327a832bdc024d05ef8d2178a
Ricardo <nadalrick@gmaill.com>
2021-11-05 17:16:01 +0100

    Añadido clase Roll, y modificado player

---
+++ src/main/java/org/springframework/samples/petclinic/dice/Roll.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.dice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Roll {
+
+    private Integer rollAmount;
+    private List<DiceValues> DiceValues;
+
+    public Roll() {
+        rollAmount=0;
+        DiceValues=rollDice();
+        
+    }
+    
+    public List<DiceValues> rollDice(){
+        List<DiceValues> resultado=new ArrayList<DiceValues>();
+        Integer tiradas=6;
+        int i;
+        int max=5;
+        int min=0;
+        for(i=0;i<tiradas;i++) {
+            Integer valor=(int)Math.floor(Math.random()*(max-min+1)+min);
+            resultado.add(DiceValues.get(valor));
+        }
+        return resultado; 
+    }
+}
