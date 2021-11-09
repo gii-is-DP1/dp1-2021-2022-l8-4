4073a0051b7996c3a063a5d825ba71f266d28f61
Rosa <rosamolina100@gmail.com>
2021-11-09 18:46:24 +0100

    Creado formulario para usuarios

--- src/main/java/org/springframework/samples/petclinic/userKoT/UserControllerKoT.java
+++ src/main/java/org/springframework/samples/petclinic/userKoT/UserControllerKoT.java
@@ -1,9 +1,17 @@
 package org.springframework.samples.petclinic.userKoT;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,7 +25,7 @@ public class UserControllerKoT {
 	@Autowired
 	private UserServiceKoT userService;
     
-   // private static final String VIEWS_USERS_CREATE_FORM = "users/usersList";
+   private static final String VIEWS_USERS_KOT_CREATE_FORM = "userskot/createUserskotForm";
 
 	@GetMapping()
     public String usersList(ModelMap modelMap){
@@ -26,4 +34,30 @@ public class UserControllerKoT {
         modelMap.addAttribute("userskot", userskot);
         return view;
     }
+
+/**
+ * FORMULARIO PARA USUARIOS
+ */    
+    @GetMapping(path = "/new")
+    public String initCreationForm(ModelMap modelMap) {
+        String view = VIEWS_USERS_KOT_CREATE_FORM;
+        modelMap.addAttribute("userkot", new UserKoT());
+        return view;
+    }
+
+    @PostMapping(path = "/new")
+    public String processCreationForm(@Valid UserKoT userkot, BindingResult result, ModelMap modelMap) {
+        String view="/userkot";
+        if (result.hasErrors()){
+            modelMap.addAttribute("userkot", userkot);
+            return VIEWS_USERS_KOT_CREATE_FORM;
+
+        }else {
+            //creating user
+            userService.saveUser(userkot);
+            modelMap.addAttribute("message","User succesfully created!");
+            view = usersList(modelMap);
+        }
+        return view;
+    } 
 }
