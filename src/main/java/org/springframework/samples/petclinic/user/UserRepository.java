378b102f5933c74558d3acf9c9215e684ac45b40
Carlos <carlosvsoult@gmail.com>
2021-11-19 10:53:03 +0100

    Trabajando en H9 - Listado de usuarios registrados por paginación

--- src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
@@ -1,8 +1,17 @@
 package org.springframework.samples.petclinic.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author Carlos Varela Soult
+ */
 
 public interface UserRepository extends  CrudRepository<User, String>{
 	
+    // H9 - Listado de usuarios registrados por paginación
+    @Query(value="SELECT * FROM Users", nativeQuery=true)
+    Page<User> findUserByPage(Pageable pageable);
 }
