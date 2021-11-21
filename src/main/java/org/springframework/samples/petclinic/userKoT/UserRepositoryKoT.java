a2c548cf264e83f512973a899af90e21ac810508
Carlos <carlosvsoult@gmail.com>
2021-11-21 17:51:58 +0100

    actualizar

--- src/main/java/org/springframework/samples/petclinic/userKoT/UserRepositoryKoT.java
+++ src/main/java/org/springframework/samples/petclinic/userKoT/UserRepositoryKoT.java
@@ -1,8 +1,6 @@
 package org.springframework.samples.petclinic.userKoT;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
 /**
@@ -12,10 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 */
 
 public interface UserRepositoryKoT extends CrudRepository<UserKoT, Integer>{
-
-    // H9 - Listado de usuarios registrados por paginación
-    @Query(value="SELECT * FROM UsersKoT", nativeQuery=true)
-    Page<UserKoT> findUserByPage(Pageable pageable);
 
     // Obtener currentUser a partir de username
     @Query(value="SELECT * FROM USERSKOT WHERE username=?1", nativeQuery=true)
--- src/main/java/org/springframework/samples/petclinic/userKoT/UserServiceKoT.java
+++ src/main/java/org/springframework/samples/petclinic/userKoT/UserServiceKoT.java
@@ -55,7 +55,4 @@ public class UserServiceKoT{
 		Integer currentUserId = currentUserKoT.getId();
 		return currentUserId;
 	}
-	
-	
-
 }
