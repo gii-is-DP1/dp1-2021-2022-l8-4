a0794a35828e019daed3b86fa1230f4e400b1b6e
Josdelsan <joseM_DS@hotmail.com>
2021-12-18 15:26:17 +0100

    Arreglada pagina de rakings

--- src/main/java/org/springframework/samples/petclinic/modules/statistics/metrics/MetricControler.java
+++ src/main/java/org/springframework/samples/petclinic/modules/statistics/metrics/MetricControler.java
@@ -3,13 +3,12 @@ package org.springframework.samples.petclinic.modules.statistics.metrics;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.modules.statistics.achievement.Achievement;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.Map;
 
 
@@ -24,31 +23,16 @@ public class MetricControler {
     private MetricService metricService;
 
     @GetMapping()
-    public String getStatistics(ModelMap modelMap){
+    public String getRanking(@RequestParam(value = "metric", defaultValue = "gamesPlayed") MetricType metric, ModelMap modelMap){
         String view = "modules/statistics/metrics/metrics";
-        MetricType metric = MetricType.gamesPlayed;
-        List<MetricData> rows = metricService.statisticsByMetricType(metric);
-
-        modelMap.addAttribute("metrics", MetricType.values());
-        modelMap.addAttribute("metric", metric);
-        modelMap.addAttribute("metricToDisplay", new Achievement());
-        modelMap.addAttribute("rows", rows);
-        return view;
-    }
-
-    @PostMapping
-    public String requestStatistics(@ModelAttribute("metricToDisplay") Achievement metricToDisplay,ModelMap modelMap){
-        String view = "modules/statistics/metrics/metrics";
-        MetricType metric = metricToDisplay.getMetric();
         List<MetricData> rows = metricService.statisticsByMetricType(metric);
 
         modelMap.addAttribute("metrics", MetricType.values());
         modelMap.addAttribute("actualMetric", metric);
-        modelMap.addAttribute("metricToDisplay", new Achievement());
         modelMap.addAttribute("rows", rows);
         return view;
-
     }
+
     
     @GetMapping("/statistics")
     public String rules(Map<String, Object> model) {	    
