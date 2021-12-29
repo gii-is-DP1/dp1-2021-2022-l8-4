e7776c076d3f929d34f6343234ee78a217b56a5f
lNoelia <noelialopezduran@gmail.com>
2021-12-29 18:33:16 +0100

    merge master

--- src/main/java/org/springframework/samples/petclinic/card/UseCardsInterface.java
+++ src/main/java/org/springframework/samples/petclinic/card/UseCardsInterface.java
@@ -1,29 +1,30 @@
 package org.springframework.samples.petclinic.card;
 
+import org.springframework.samples.petclinic.game.MapGameRepository;
 import org.springframework.samples.petclinic.player.Player;
 import org.springframework.samples.petclinic.player.PlayerService;
 
 public interface UseCardsInterface {
     
-    default void effect(Player player,PlayerService playerService) {
+    default void effect(Player player,PlayerService playerService, MapGameRepository mapGameRepository) {
         int doNothing=0;
     }
-    default void effectStartTurn(Player player,PlayerService playerService) {
+    default void effectStartTurn(Player player,PlayerService playerService, MapGameRepository mapGameRepository) {
         int doNothing=0;
     }
-    default void effectEndTurn(Player player,PlayerService playerService) {
+    default void effectEndTurn(Player player,PlayerService playerService, MapGameRepository mapGameRepository) {
         int doNothing=0;
     }
-    default void effectInRoll(Player player,PlayerService playerService) {
+    default void effectInRoll(Player player,PlayerService playerService, MapGameRepository mapGameRepository) {
         int doNothing=0;
     }
-    default void effectAfterRoll(Player player,PlayerService playerService) {
+    default void effectAfterRoll(Player player,PlayerService playerService, MapGameRepository mapGameRepository) {
         int doNothing=0;
     }
-    default Integer effectDamage(Player player,PlayerService playerService,Integer damage) {
+    default Integer effectDamage(Player player,PlayerService playerService,Integer damage, MapGameRepository mapGameRepository) {
         return damage;
     }
-    default Integer effectBuy(Player player,PlayerService playerService,Integer energy,Integer cost) {
+    default Integer effectBuy(Player player,PlayerService playerService,Integer energy,Integer cost, MapGameRepository mapGameRepository) {
         return energy;
     }
 
