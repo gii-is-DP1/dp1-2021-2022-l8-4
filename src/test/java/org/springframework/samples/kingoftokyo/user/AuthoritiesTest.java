b2cc4c010f58fb5a98b0764e79d95bcb7f188d4f
Rosa <rosamolina100@gmail.com>
2022-01-18 15:54:19 +0100

    arreglando code smells 2

--- src/test/java/org/springframework/samples/kingoftokyo/user/AuthoritiesTest.java
+++ src/test/java/org/springframework/samples/kingoftokyo/user/AuthoritiesTest.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 
-public class AuthoritiesTest {
+class AuthoritiesTest {
  
     
     @Autowired
