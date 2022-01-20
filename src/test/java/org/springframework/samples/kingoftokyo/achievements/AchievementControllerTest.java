769a3e3da2a2244d067baa8622c0c851d66c3679
Rosa <rosamolina100@gmail.com>
2022-01-20 17:40:39 +0100

    borrando imports

--- src/test/java/org/springframework/samples/kingoftokyo/achievements/AchievementControllerTest.java
+++ src/test/java/org/springframework/samples/kingoftokyo/achievements/AchievementControllerTest.java
@@ -1,33 +1,26 @@
 package org.springframework.samples.kingoftokyo.achievements;
 
-import static org.mockito.ArgumentMatchers.any;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.kingoftokyo.configuration.CurrentUserController;
 import org.springframework.samples.kingoftokyo.configuration.SecurityConfiguration;
-import org.springframework.samples.kingoftokyo.modules.statistics.achievement.Achievement;
 import org.springframework.samples.kingoftokyo.modules.statistics.achievement.AchievementController;
 import org.springframework.samples.kingoftokyo.modules.statistics.achievement.AchievementService;
-import org.springframework.samples.kingoftokyo.modules.statistics.metrics.MetricType;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
 
 
 /**
